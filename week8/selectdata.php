4eb2b4af874af2df8d6e2200e47ffaa6f2f8a4e9
Jorenzo Martin Reyes <jjreyes@student.apc.edu.ph>
2024-02-08 15:17:36 +0800

    fixed selectdata

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT name,email,website,comment,gender FROM jjreyes_myguests";
+$sql = "SELECT id,name,email,website,comment,gender FROM jjreyes_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
