39fadf000ef5695e229ebc5e49a23b8245d515a0
Jorenzo Martin Reyes <jjreyes@student.apc.edu.ph>
2024-02-08 15:14:53 +0800

    fixed selectdata

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT name,email,website,comment,gender FROM jjreyes_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "name: " . $row["name"]. " - email: " . $row["email"]. "website: " . $row["comment"]. "comment: " . $row["gender"]. "gender: " . $row["website"]. "<br>";
   }
 } else {
   echo "0 results";
